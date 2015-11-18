02663ad5344cd030aa330d1c1d425d245004ad9d
André Gumieri <andre.gumieri@gumini.com.br>
2015-11-18 14:39:41 -0200

    Removido o arquivo de task inutilizado do browser-sync
    O BrowserSync executa junto com as tasks default, por este motivo
    não há mais necessidade de te-lo na pasta.

--- gulp/tasks/browser-sync.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require("gulp");
-var config = require('../config');
-var browserSync = require("browser-sync");
-
-gulp.task('browser-sync', function() {
-	browserSync.init(config.browserSync.conf);
-});
